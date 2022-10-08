35219cbf527dd4367009351bea44ed4da17a67c9
valdemir <valdemirdevops@gmail.com>
2022-10-07 22:28:07 -0300

    criação do script e atualização do servidor apach

---
+++ Script_2.sh
@@ -0,0 +1,19 @@
+!#/bin/bash
+
+echo "Atualizando o servidor..."
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando e copiando os arquivos da aplicação..."
+
+cd /tmp
+wget https://github.com/denilsobonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip mains.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+echo "Fim"
+
